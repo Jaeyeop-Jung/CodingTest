1960a2a86a098035549aa663979bd48b351f0866
Jaeyeop-Jung <jjy5349@naver.com>
2022-03-28 10:58:22 +0900

    2022-03-28

---
+++ 프로그래머스/숫자 문자열과 영단어.py
@@ -0,0 +1,9 @@
+def solution(s):
+    answer = s
+    dic = {'zero':0, 'one':1, 'two':2, 'three':3, 'four':4, 'five':5, 'six':6, 'seven':7, 'eight':8,'nine':9}
+
+    keys = dic.keys()
+    for i in keys:
+        answer = answer.replace(i, str(dic[i]))
+
+    return int(answer)
