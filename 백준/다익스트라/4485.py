7ae90da2534bb08380e9d84e2f611802b06f3666
Jaeyeop-Jung <jjy5349@naver.com>
2022-04-11 11:25:01 +0900

    2022-04-11

---
+++ 백준/다익스트라/4485.py
@@ -0,0 +1,50 @@
+import sys
+import heapq
+INF = int(1e9)
+input = sys.stdin.readline
+
+
+result = []
+while True:
+
+    n = int(input())
+    if n == 0:
+        break
+
+    graph = []
+    for i in range(n):
+        graph.append(list(map(int, input().split())))
+
+    def dijkstra():
+        heap = []
+        distance = []
+        for i in range(n):
+            distance.append([INF] * n)
+        dRow = [0, 0, 1, -1]
+        dColumn = [1, -1, 0, 0]
+
+        heapq.heappush(heap, [0, 0])
+        distance[0][0] = graph[0][0]
+        while heap:
+            row, column = heapq.heappop(heap)
+            dist = distance[row][column]
+
+            if distance[row][column] < dist:
+                continue
+
+            for i in range(4):
+                movedRow, movedColumn = row + dRow[i], column + dColumn[i]
+                if movedRow < 0 or n <= movedRow or movedColumn < 0 or n <= movedColumn:
+                    continue
+
+                cost = dist + graph[movedRow][movedColumn]
+                if cost < distance[movedRow][movedColumn]:
+                    distance[movedRow][movedColumn] = cost
+                    heapq.heappush(heap, [movedRow, movedColumn])
+
+        return distance
+
+    result.append(dijkstra()[n-1][n-1])
+
+for i in range(len(result)):
+    print(f'Problem {i+1}: {result[i]}')
