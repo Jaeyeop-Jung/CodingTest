491c14d3d3d8c8521beb45f26118ce3f8afdb12c
Jaeyeop-Jung <jjy5349@naver.com>
2022-04-14 09:27:41 +0900

    2022-04-14

--- 백준/위상 정렬/1005.py
+++ 백준/위상 정렬/1005.py
@@ -1,41 +1,34 @@
+import sys
 from collections import deque
+input = sys.stdin.readline
 
 t = int(input())
 for i in range(t):
     n, k = map(int, input().split())
     d = list(map(int, input().split()))
     d.insert(0, 0)
-    graph = [[] for i in range(n + 1)]
+    graph = [[] for j in range(n + 1)]
     indegree = [0] * (n + 1)
-    for i in range(k):
+    for j in range(k):
         x, y = map(int, input().split())
         graph[x].append(y)
         indegree[y] += 1
     w = int(input())
 
-    result = []
     dp = [0] * (n + 1)
     q = deque()
-    for i in range(n + 1):
-        if indegree[i] == 0:
-            q.append(i)
-            dp[i] += d[i]
+    for j in range(1, n + 1):
+        if indegree[j] == 0:
+            q.append(j)
+            dp[j] += d[j]
 
     while q:
         now = q.popleft()
-        result.append(now)
-        max_d = 0
 
-        for i in graph[now]:
-            indegree[i] -= 1
-            if indegree[i] == 0:
-                q.append(i)
-                max_d = max(max_d, d[i])
-        for i in graph[now]:
-            dp[i] = dp[now] + max_d
-    print(dp)
+        for j in graph[now]:
+            indegree[j] -= 1
+            dp[j] = max(dp[j], dp[now] + d[j])
+            if indegree[j] == 0:
+                q.append(j)
 
-
-
-
-
+    print(dp[w])
