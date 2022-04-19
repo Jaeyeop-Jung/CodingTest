9fbaf865f367ad190aa15630544e7261088ab32e
Jaeyeop-Jung <jjy5349@naver.com>
2022-04-19 12:45:25 +0900

    2022-04-19

---
+++ 백준/최소 스패닝 트리/1922.py
@@ -0,0 +1,35 @@
+import sys
+input = sys.stdin.readline
+
+def find_parent(parent, x):
+    if parent[x] != x:
+        parent[x] = find_parent(parent, parent[x])
+    return parent[x]
+
+def union_parent(parent, x, y):
+    a = find_parent(parent, x)
+    b = find_parent(parent, y)
+    if a < b:
+        parent[b] = a
+    else:
+        parent[a] = b
+
+
+n = int(input())
+m = int(input())
+graph = []
+for i in range(m):
+    a, b, c = map(int, input().split())
+    graph.append([c, a, b])
+parent = []
+for i in range(n + 1):
+    parent.append(i)
+
+graph.sort()
+result = 0
+for i in graph:
+    if find_parent(parent, i[1]) != find_parent(parent, i[2]):
+        union_parent(parent, i[1], i[2])
+        result += i[0]
+
+print(result)
